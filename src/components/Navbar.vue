d9aa5ab019b32345b860642bf2780262a99c3ec9
Seakty <sambathseakty@gmail.com>
2025-08-06 15:58:27 +0700

    Add scroll behavior to nav bar, and add some hover and style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-    <nav class="navbar">
+    <nav :class="['navbar', { 'scrolled': isScrolled}]">
       <div class="logo">
         <a href="/">
-          <img src="/ReDa-Icon.png" alt="ReDa Lab Logo" />
+          <img :src="isScrolled ? reda_Logo_Blue : reda_Logo_Light" alt="ReDa Lab Logo" />
         </a>
       </div>
-      <ul class="nav-links">
+      <ul :class="['nav-links', { 'dark-links': isScrolled }]">
         <li><a href="/AboutUs">About us</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Events</a></li>
@@ -18,9 +18,28 @@
 </template>
 
 <script setup>
+  import { onMounted, onUnmounted, ref } from 'vue';
+
+  const isScrolled = ref(false);
+
+  const handleScroll = () => {
+    isScrolled.value = window.scrollY > 50;
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+  })
+
   defineOptions({
     name: "ComponentNavbar",
   });
+
+  const reda_Logo_Light = "/ReDa-Icon.png";
+  const reda_Logo_Blue = "/ReDa-Icon-Blue.png";
 
 </script>
 
