6f0a193442e54d4319e27ce8e94c1d3bb22f0057
Striker Eurika <ngentina69@gmail.com>
2026-02-26 15:50:58 +0700

    remove vue and replace with react

--- eslint.config.js
+++ eslint.config.js
@@ -1,56 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  globalIgnores(['dist']),
   {
-    name: "app/files-to-lint",
-    files: ["**/*.{js,mjs,jsx,ts,tsx,vue}"],
-  },
-
-  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
-
-  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
   },
-
-  js.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-
-  // TypeScript configuration
-  {
-    files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-    },
-  },
-
-  // Vue files with TypeScript
-  {
-    files: ["**/*.vue"],
-    languageOptions: {
-      parserOptions: {
-        parser: tsparser,
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-  },
-]);
+])
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-// export * from './format';
-// export * from './validators';
-// export * from './storage';
-// export * from './api';
-// export * from './textTruncation';
-// export * from './constants';
-
-export * from "./textTruncation";
