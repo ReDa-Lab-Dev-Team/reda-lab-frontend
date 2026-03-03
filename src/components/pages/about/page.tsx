ca79a70b1e12569586ce2fddecb387b33e83bbcb
Striker Eurika <ngentina69@gmail.com>
2026-03-03 22:21:27 +0700

    finish about page

--- src/components/pages/about/page.tsx
+++ src/components/pages/about/page.tsx
@@ -1,3 +1,4 @@
+import SecondaryHero from "@/components/common/secondary-hero";
 import AboutRedaSection from "./about-reda-section";
 import ResearchersSection from "./researchers-section";
 import PartnersSection from "./partners-section";
@@ -5,6 +6,11 @@ import PartnersSection from "./partners-section";
 export const AboutUs = () => {
   return (
     <>
+      <SecondaryHero
+        topText="Who We Are"
+        mainTitle="About Us"
+        bottomText="People · Purpose · Progress"
+      />
       <AboutRedaSection />;
       <ResearchersSection />;
       <PartnersSection />;
