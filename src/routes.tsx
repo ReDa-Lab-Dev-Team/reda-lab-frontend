806a8972e604560c24ed796b9dff6cb5cbbd1afb
Striker Eurika <ngentina69@gmail.com>
2026-03-06 15:57:02 +0700

    add not found page (done)

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/main-layout";
 import HomePage from "./pages/home-page";
 import AboutPage from "./pages/about-page";
 import ResearchPage from "./pages/research-page";
+import NotFoundPage from "./pages/not-found-page";
 
 export const router = createBrowserRouter([
   {
@@ -13,25 +14,17 @@ export const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
-    ],
-  },
-  {
-    path: "/about",
-    element: <MainLayout />,
-    children: [
       {
-        index: true,
+        path: "about",
         element: <AboutPage />,
       },
-    ],
-  },
-  {
-    path: "/research",
-    element: <MainLayout />,
-    children: [
       {
-        index: true,
+        path: "research",
         element: <ResearchPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
       },
     ],
   },
